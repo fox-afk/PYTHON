83dcd48b2d62c2bba92b1c2c2fef7a52e8b1414c
fox-afk <brunoaraujobrito@hotmail.com>
2023-07-19 08:21:06 -0300

    py

---
+++ ex4.8.py
@@ -0,0 +1,7 @@
+#Um programa que pergunte qual operação matematica que você deseja realizar
+
+operação= (input("Digite a operação matetematica desejada:  "))
+numero1 = int (input("Digite um numero   ")) 
+numero2 = int (input("Digite um numero:  ")) 
+
+print(f'{numero1} {numero2}')
---
+++ ex4.9.py
@@ -0,0 +1,12 @@
+# Programa que aprove imprestimo bancario para comprar uma casa. O programa deve perguntar o valor da casa a comprar
+#   o salario e a quantidade de anos a pagar. o valor da prestação mensal não pode ser superior a 30% do salario.
+#     Calcule a prestação como sendo o valor da casa a comprar divido pelo numero de meses a pagar
+
+
+casa    = float (input("Qual o valor da casa? R$: "))
+salario = float (input("Qual e o seu salario  R$: "))
+
+
+
+
+print(f'O valor da casa é de R$:{casa}, o salario é de R$:{salario}')
