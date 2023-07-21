bc4f11f98f00f6d9d7fd11ad0eaf298a28f6db15
fox-afk <brunoaraujobrito@hotmail.com>
2023-07-21 12:19:52 -0300

    py

--- ex4.9.py
+++ ex4.9.py
@@ -5,8 +5,12 @@
 
 casa    = float (input("Qual o valor da casa? R$: "))
 salario = float (input("Qual e o seu salario  R$: "))
+anos = int (input("Quantos anos demora para pagar a casa?"))
+
+valo_casa= casa / anos 
 
 
-
-
-print(f'O valor da casa é de R$:{casa}, o salario é de R$:{salario}')
+print(f'O valor da casa é de R$:{casa}')
+print(f'o salario é de R$:{salario}')
+print(f'demora {anos} anos')
+print(f'{valo_casa}')
