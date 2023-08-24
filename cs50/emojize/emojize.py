1f1b6b9fb4c669dec6fcb6942ed45f369c301022
fox-afk <brunoaraujobrito@hotmail.com>
2023-08-24 11:49:04 -0300

    py

--- cs50/emojize/emojize.py
+++
@@ -1,5 +0,0 @@
-import emoji
-user_answer=input("input: ")
-
-output=emoji.emojize(user_answer)
-print("output: ",output)
--- cs50/twttr/twttr.py
+++
@@ -1,6 +0,0 @@
-answer= input("input: ")
-print("output: ", end="")
-for letter in answer:
-    if not letter.lower() in ['a','e','i','o','u']:
-        print(letter,end="")    
-print()
